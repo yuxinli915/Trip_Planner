63c6d758543b3fd98c0393e0727ab5ae06827765
Yuxin Li <yuxinli915@outlook.com>
2020-05-21 16:02:09 -0500

    Update destination results on DOM. A result can be selected when user clicks on it. Use one function to handle both search.

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,57 @@
 const apiKey = `pk.eyJ1Ijoic3VwZXJ4aW4iLCJhIjoiY2thNWlqZHd4MDBpODNnb3owMDA2Y3dnYiJ9.-zciQf0emsOdjhIjB2uoNA`;
 const originForm = document.querySelector(`.origin-form`);
+const destinationForm = document.querySelector(`.destination-form`);
 const originResultsList = document.querySelector(`.origins`);
+const destinationResultsList = document.querySelector(`.destinations`);
+const btn = document.querySelector(`.plan-trip`);
 
 originForm.addEventListener(`submit`, event => {
   const originInput = originForm.querySelector(`input`);
 
   if (originInput.value.trim() !== ``) {
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${originInput.value}.json?access_token=${apiKey}&limit=10&bbox=-97.325875,49.766204,-96.953987,49.99275`)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error(`Fail to get data from API.`);
-        }
-      })
-      .then(data => updateOriginResults(data.features));
+    searchLocations(originInput.value, originResultsList);
   }
 
   originInput.value = ``;
   event.preventDefault();
 })
 
-function updateOriginResults(locationsList) {
+destinationForm.addEventListener(`submit`, event => {
+  const destinationInput = destinationForm.querySelector(`input`);
+
+  if (destinationInput.value.trim() !== ``) {
+    searchLocations(destinationInput.value, destinationResultsList);
+  }
+
+  destinationInput.value = ``;
+  event.preventDefault();
+})
+
+originResultsList.addEventListener(`click`, event => {
+  clickResult(event);
+})
+
+destinationResultsList.addEventListener(`click`, event => {
+  clickResult(event);
+})
+
+btn.addEventListener(`click`, () => {
+
+})
+
+function searchLocations(keyword, listEle) {
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${keyword}.json?access_token=${apiKey}&limit=10&bbox=-97.325875,49.766204,-96.953987,49.99275`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Fail to get data from API.`);
+      }
+    })
+    .then(data => updateResults(data.features, listEle));
+}
+
+function updateResults(locationsList, listEle) {
   let html = ``;
 
   locationsList.forEach(location => {
@@ -37,14 +67,14 @@ function updateOriginResults(locationsList) {
     `;
   })
 
-  originResultsList.innerHTML = html;
+  listEle.innerHTML = html;
 }
 
-originResultsList.addEventListener(`click`, event => {
+function clickResult(event) {
   if (event.target.closest(`li`) !== null) {
-    const originResults = document.querySelectorAll(`.origins > li`);
+    const results = document.querySelectorAll(`${event.target.closest(`ul`)} > li`);
 
-    originResults.forEach(result => result.classList.remove(`selected`));
+    results.forEach(result => result.classList.remove(`selected`));
     event.target.closest(`li`).classList.add(`selected`);
   }
-})
+}
